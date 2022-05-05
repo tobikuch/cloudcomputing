010d9ee378e237195975bafb9d758b3f80a1d5ca
tobikuch <kuch@hm.edu>
2022-05-05 13:34:31 +0200

    Changed Script

---
+++ 04-iaas/uebung/teil-2/.terraform.lock.hcl
@@ -0,0 +1,38 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "3.12.0"
+  constraints = ">= 2.68.0, 3.12.0"
+  hashes = [
+    "h1:MpNA4IQVVi1INZAqWcJ6I3F9Q8lxjabtMEhcvoSfcVI=",
+    "zh:002f7c17f927978e06c5c6ff7dd3f4600f0ab7ec7e43e62eedfce2b22c70b009",
+    "zh:0400f415121efad618d9ddca71522eb4d0958905e705daa6ecd9f7d8c1330253",
+    "zh:285e630188f7d0fd3d1e6f715326723904f02a2fa571e40dde36ae1b5cd5542e",
+    "zh:402140c0a7d0423ff7c49b6bd03769332b8beb41838bf58eb83b7cfa73f67e09",
+    "zh:7506eadb178fa41c02a0127e06ab4a49f2604860f0992f7c4f519993932d2a41",
+    "zh:95dffd98bad81a2288953f36250f358ab9560d6bfb249ff5d00bf4f3131cc08b",
+    "zh:aa530b43e4a2ff89025d8b4ed2384b67cadedbf84c67ee903bac133376d929b9",
+    "zh:b2314b788a6a7091f4d70bff3825077e06c9c994b1efe76ad5f0e4b60944e60b",
+    "zh:b68d3df21d7d67641e08476bd456d04bd33d963caba41024a14dddd85b0bde10",
+    "zh:b9a8204ce6ab4929145cd2a284ba89790813692c6de9c8e12be9c1d984989180",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version     = "3.0.0"
+  constraints = "3.0.0"
+  hashes = [
+    "h1:grDzxfnOdFXi90FRIIwP/ZrCzirJ/SfsGBe6cE0Shg4=",
+    "zh:0fcb00ff8b87dcac1b0ee10831e47e0203a6c46aafd76cb140ba2bab81f02c6b",
+    "zh:123c984c0e04bad910c421028d18aa2ca4af25a153264aef747521f4e7c36a17",
+    "zh:287443bc6fd7fa9a4341dec235589293cbcc6e467a042ae225fd5d161e4e68dc",
+    "zh:2c1be5596dd3cca4859466885eaedf0345c8e7628503872610629e275d71b0d2",
+    "zh:684a2ef6f415287944a3d966c4c8cee82c20e393e096e2f7cdcb4b2528407f6b",
+    "zh:7625ccbc6ff17c2d5360ff2af7f9261c3f213765642dcd84e84ae02a3768fd51",
+    "zh:9a60811ab9e6a5bfa6352fbb943bb530acb6198282a49373283a8fa3aa2b43fc",
+    "zh:c73e0eaeea6c65b1cf5098b101d51a2789b054201ce7986a6d206a9e2dacaefd",
+    "zh:e8f9ed41ac83dbe407de9f0206ef1148204a0d51ba240318af801ffb3ee5f578",
+    "zh:fbdd0684e62563d3ac33425b0ac9439d543a3942465f4b26582bcfabcb149515",
+  ]
+}
--- 04-iaas/uebung/teil-2/loadbalancer.tf
+++ 04-iaas/uebung/teil-2/loadbalancer.tf
@@ -1,8 +1,33 @@
 # TODO: Create a Security Group for the Load Balancer
 # See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
 # TODO: Allow incoming traffic on the Load Balancer on port 80 from everywhere 
-# See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule 
+# See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
 
 # TODO: Allow outgoing traffic from the LB to everywhere
 # See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule 
@@ -11,38 +36,42 @@
 # TODO: Create the Load Balancer
 # See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
 
-# resource "aws_lb" "app" {
-#   name               =
-#   load_balancer_type = "application"
-#   security_groups    = 
-#   subnets            = 
-#   tags               = local.standard_tags
-# }
+resource "aws_lb" "loadbalancer" {
+    name               = "loadbalancer"
+    load_balancer_type = "application"
+    security_groups    = [aws_security_group.allow_http.id]
+    subnets            = module.vpc.public_subnets
+    tags               = local.standard_tags
+}
 
 # TODO: Create a target group
 # See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
 
-# resource "aws_lb_target_group" "app" {
-#   name                 = 
-#   port                 = 
-#   protocol             = 
-#   vpc_id               = 
-#   deregistration_delay = 60
+resource "aws_lb_target_group" "app_target" {
+   name                 = "apptarget"
+   port                 = 80
+   protocol             = "HTTP"
+   vpc_id               = module.vpc.vpc_id
+   deregistration_delay = 60
 
-#   health_check {
-#     interval = 6
-#     path     = 
-#     port     = 
-#     protocol = 
-#   }
-# }
+   health_check {
+     interval = 6
+     path     = "/"
+     port     = 8080
+     protocol = "HTTP"
+   }
+ }
 
 # TODO: Create a listener
 # See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
 
-# resource "aws_lb_listener" "lb_forward_to_app" {
-#   ...
-#   default_action {
-#   }
-# }
+resource "aws_lb_listener" "lb_forward_to_app" {
+    load_balancer_arn = aws_lb.loadbalancer.arn
+    port              = "80"
+    protocol          = "HTTP"
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.app_target.arn
+        }
+}
 
